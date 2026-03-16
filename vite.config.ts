e43c6f85168a9974c8586e336fe9ba33e33a0454
HiggaNi <93601498+Wapiy-y@users.noreply.github.com>
2026-03-16 22:46:50 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -28,12 +28,18 @@ export default defineConfig({
 						type: "image/png",
 					},
 					{
+						src: "/icon-192-maskable.png",
+						sizes: "192x192",
+						type: "image/png",
+						purpose: "maskable",
+					},
+					{
 						src: "/icon-512.png",
 						sizes: "512x512",
 						type: "image/png",
 					},
 					{
-						src: "/icon-512.png",
+						src: "/icon-512-maskable.png",
 						sizes: "512x512",
 						type: "image/png",
 						purpose: "maskable",
